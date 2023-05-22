6710ed0d10de9e3f82b861856b0b27d9699940c0
sathwik_karne <karnesathwik@gmail.com>
2023-05-22 17:52:51 +0530

    Update implementation.py

--- implementation.py
+++ implementation.py
@@ -12,15 +12,15 @@ packer.add_bin(Bin('large-2-box', 23.6875, 11.75, 3.0, 70.0))
 
 
 
-packer.add_item(Item('50g [item 1]', 3.9370, 1.9685, 1.9685, 1))
-packer.add_item(Item('50g [item 2]', 3.9370, 1.9685, 1.9685, 2))
-packer.add_item(Item('50g [item 3]', 3.9370, 1.9685, 1.9685, 3))
-packer.add_item(Item('250g [item 4]', 7.8740, 3.9370, 1.9685, 4))
-packer.add_item(Item('250g [item 5]', 7.8740, 3.9370, 1.9685, 5))
-packer.add_item(Item('250g [item 6]', 7.8740, 3.9370, 1.9685, 6))
-packer.add_item(Item('250g [item 7]', 7.8740, 3.9370, 1.9685, 7))
-packer.add_item(Item('250g [item 8]', 7.8740, 3.9370, 1.9685, 8))
-packer.add_item(Item('250g [item 9]', 7.8740, 3.9370, 1.9685, 9))
+packer.add_item(Item('[item 1]', 3.9370, 1.9685, 1.9685, 1))
+packer.add_item(Item('[item 2]', 3.9370, 1.9685, 1.9685, 2))
+packer.add_item(Item('[item 3]', 3.9370, 1.9685, 1.9685, 3))
+packer.add_item(Item('[item 4]', 7.8740, 3.9370, 1.9685, 4))
+packer.add_item(Item('[item 5]', 7.8740, 3.9370, 1.9685, 5))
+packer.add_item(Item('[item 6]', 7.8740, 3.9370, 1.9685, 6))
+packer.add_item(Item('[item 7]', 7.8740, 3.9370, 1.9685, 7))
+packer.add_item(Item('[item 8]', 7.8740, 3.9370, 1.9685, 8))
+packer.add_item(Item('[item 9]', 7.8740, 3.9370, 1.9685, 9))
 
 
 
